d981bee21b287e5d0811727efd6fdb20f55b4712
f531 <0xf531@gmail.com>
2021-05-12 23:30:44 +0200

    trying to fix issues with typescript and npm

--- lib/ensure.ts
+++ lib/ensure.ts
@@ -2,13 +2,13 @@ interface Ens<K extends keyof Ens<K, V>, V> {
     [key: string]: V
 }
 
-export function ensure<K extends keyof Ens<K, V>, V>(object: Ens<K, V>, condition: (it: V) => boolean = it => !!it) {
+export function ensure<K extends keyof Ens<K, V>, V>(object: Ens<K, V>, condition: (it: V) => boolean = () => true) {
     const entries = Object.entries(object)
     if (entries.length !== 1) {
         throw new Error(`ENSURE: Illegal usage of 'ensure' function!`)
     }
     const [entry] = entries
-    if (!entry[1] || !condition(entry[1])) {
+    if (typeof entry[1] === 'undefined' || !condition(entry[1])) {
         throw new Error(`ENSURE: Illegal argument: ${entry[0]} = ${entry[1]} did not meet the given requirements!`)
     }
 }
